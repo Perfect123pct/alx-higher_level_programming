476d8994919c9e035e8a392f7acd8d2ee3bfc2d0
Adekola Oyewole <oyewoleadekolastephen@gmail.com>
2023-08-16 12:47:25 -0700

    SQL

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,4 @@
-/*
-Write a script that lists all Comedy shows in the database hbtn_0d_tvshows
-*/
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows
 
 SELECT title FROM tv_shows
 JOIN tv_show_genres ON id=tv_show_genres.show_id
