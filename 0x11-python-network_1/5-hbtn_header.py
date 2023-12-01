05b7606edacd3f76cac2ee120059cf3f2b64ac92
Perfect123pct <oyewoleadekolastephen@gmail.com>
2023-12-01 12:06:04 -0800

    Commit

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
 import requests
+import sys
 
-url = "https://alx-intranet.hbtn.io/status"
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
 
 try:
     response = requests.get(url)
     response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
 
-    body = response.text
-
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print(x_request_id)
+    else:
+        print("X-Request-Id not found in the response header.")
+except requests.exceptions.HTTPError as e:
+    print(f"Error code: {e.response.status_code}")
 except requests.exceptions.RequestException as e:
     print(f"Error: {e}")
